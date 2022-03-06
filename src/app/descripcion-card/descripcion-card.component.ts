c432100ca9a6c5c050a47356f7eb1657a1a9c05a
nerfshacopls <dsjgdavid@gmail.com>
2022-03-06 21:24:57 +0100

    descripcion-card con imagen a traves de fr-central

--- src/app/descripcion-card/descripcion-card.component.ts
+++ src/app/descripcion-card/descripcion-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-descripcion-card',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./descripcion-card.component.scss']
 })
 export class DescripcionCardComponent implements OnInit {
-
+  @Input("imagen") 
+  url_img: String = "";
   constructor() { }
 
   ngOnInit(): void {
--- src/app/frame-central/frame-central.component.ts
+++ src/app/frame-central/frame-central.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-frame-central',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrameCentralComponent implements OnInit {
 
+  url_imagen = "https://cdn.pixabay.com/photo/2016/04/20/08/21/entrepreneur-1340649_960_720.jpg";
   constructor() { }
 
   ngOnInit(): void {
+    
   }
-
+  
 }
