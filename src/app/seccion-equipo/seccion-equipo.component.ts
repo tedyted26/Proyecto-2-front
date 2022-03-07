14f01c3ea116171e6505259699d7eb9af1728e84
SantiagoBarreiroCampos <72943433+SantiagoBarreiroCampos@users.noreply.github.com>
2022-03-07 19:48:48 +0100

    Equipo con fotillos

--- src/app/seccion-equipo/seccion-equipo.component.ts
+++ src/app/seccion-equipo/seccion-equipo.component.ts
@@ -8,11 +8,11 @@ import { IntegranteModel } from '../modelos/integrante-info-model.model';
 })
 export class SeccionEquipoComponent implements OnInit {
   integrantes = [
-    new IntegranteModel("https://cdn-icons-png.flaticon.com/512/1361/1361728.png", "Santiago Barreiro", "Jefe de equipo", "Estoy a tope"),
-    new IntegranteModel("https://cdn-icons-png.flaticon.com/512/1361/1361728.png", "2Santiago Barreiro", "Jefe de equipo", "Estoy a tope"),
-    new IntegranteModel("https://cdn-icons-png.flaticon.com/512/1361/1361728.png", "3Santiago Barreiro", "Jefe de equipo", "Estoy a tope"),
-    new IntegranteModel("https://cdn-icons-png.flaticon.com/512/1361/1361728.png", "4Santiago Barreiro", "Jefe de equipo", "Estoy a tope"),
-    new IntegranteModel("https://cdn-icons-png.flaticon.com/512/1361/1361728.png", "5Santiago Barreiro", "Jefe de equipo", "Estoy a tope")
+    new IntegranteModel("assets/images/VictorArribas.jpeg", "Víctor Arribas", "Jefe de equipo", "Estoy a tope"),
+    new IntegranteModel("assets/images/SantiagoBarreiro.jpg", "Santiago Barreiro", "Jefe de equipo", "Estoy a tope"),
+    new IntegranteModel("https://cdn-icons-png.flaticon.com/512/1361/1361728.png", "Teodora Nikolaeva", "Jefe de equipo", "Estoy a tope"),
+    new IntegranteModel("assets/images/DavidSanJose.jpeg", "David San José", "Jefe de equipo", "Estoy a tope"),
+    new IntegranteModel("https://cdn-icons-png.flaticon.com/512/1361/1361728.png", "Andrés Felipe Pérez", "Jefe de equipo", "Estoy a tope")
 ];
   constructor() { }
 
