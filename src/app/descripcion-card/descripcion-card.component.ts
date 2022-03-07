33eaed938bd5289d180ca4ce478df00a1404374d
nerfshacopls <dsjgdavid@gmail.com>
2022-03-07 16:05:03 +0100

    flex grid funciona

--- src/app/descripcion-card/descripcion-card.component.ts
+++ src/app/descripcion-card/descripcion-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-descripcion-card',
   templateUrl: './descripcion-card.component.html',
-  styleUrls: ['./descripcion-card.component.scss']
+  styleUrls: ['./descripcion-card.component.scss',"../app.component.scss"]
 })
 export class DescripcionCardComponent implements OnInit {
   @Input("imagen") 
