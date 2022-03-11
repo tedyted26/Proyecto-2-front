cb9c9a4e8bb3314aa5d301ba98f1c23e2b24a2cf
VictorArribas <victorarribasgarcia@gmail.com>
2022-03-11 14:13:21 +0100

    Navbar y footer hechos, MVV añadido

--- src/app/frame-central/frame-central.component.ts
+++ src/app/frame-central/frame-central.component.ts
@@ -9,11 +9,11 @@ import { CardInfoModel } from '../modelos/card-info-model.model';
 export class FrameCentralComponent implements OnInit {
   cards_desc = [
     new CardInfoModel("https://cdn.pixabay.com/photo/2016/04/20/08/21/entrepreneur-1340649_960_720.jpg",
-  "Este es el primer titulo","Y el primer texto asdf asf asdf afs"),
+  "Misión","Ahorrar tiempo y dinero a nuestros consumidores a la hora de realizar un viaje además de garantizarles la seguridad que necesitan."),
   new CardInfoModel("https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_960_720.jpg",
-  "Este es el segundo titulo","Y el segundo texto"),
+  "Visión","Convertirse en la plataforma de organización de viajes número uno de españa en cuanto al cuidado y atención al cliente gracias a nuestro análisis intensivo de los destinos con respecto a las necesidades de cada usuario."),
   new CardInfoModel("https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_960_720.jpg",
-  "Este es el segundo titulo","Y el segundo texto"),
+  "Valores","Inclusión social, respeto al individuo, seguridad del cliente, facilidad de trámites."),
 ];
 
   constructor() { }
