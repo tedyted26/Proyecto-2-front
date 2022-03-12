39c8030abdd05e35ed53442953de0b5ab2c11971
tedyted26 <tedyted26@hotmail.com>
2022-03-12 22:01:14 +0100

    fix

--- src/app/frame-central/frame-central.component.ts
+++ src/app/frame-central/frame-central.component.ts
@@ -13,7 +13,7 @@ export class FrameCentralComponent implements OnInit {
   new CardInfoModel("https://media.istockphoto.com/photos/concept-picture-id872670490?k=20&m=872670490&s=612x612&w=0&h=2LVHEll6P7oryjVTqcJemHSjuvxersY29Nn5J0BZGik=",
   "Visión","Nos basamos en la inteligencia artificial y el análisis de sentimiento para determinar si las comunidades de los destinos escogidos por nuestros clientes pueden afectar negativamente a su integridad."),
   new CardInfoModel("https://images.pexels.com/photos/7479659/pexels-photo-7479659.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  "Valores","Inclusión social, respeto al individuo, seguridad del cliente, comodidad, facilidad de trámites."),
+  "Valores","Para nosotros siempre serán prioridades la inclusión social, el respeto al individuo, la seguridad y comodidad del cliente, la facilidad de trámites y la accesibilidad de nuestros servicios."),
 ];
 
 //https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_960_720.jpg
