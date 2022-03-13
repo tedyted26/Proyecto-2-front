99612cac6ad06daf2dccdab01f282f108d78f262
tedyted26 <tedyted26@hotmail.com>
2022-03-13 17:04:05 +0100

    merge con andres

--- src/app/frame-central/frame-central.component.ts
+++ src/app/frame-central/frame-central.component.ts
@@ -12,9 +12,9 @@ export class FrameCentralComponent implements OnInit {
     new CardInfoModel("https://images.pexels.com/photos/4553618/pexels-photo-4553618.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
   "Misión","Queremos garantizar la seguridad y comodidad de nuestros clientes a la hora de viajar, sobretodo a aquellos pertenecientes a colectivos sociales vulnerables propensos a ser víctimas de discriminaciones o delitos de odio."),
   new CardInfoModel("https://media.istockphoto.com/photos/concept-picture-id872670490?k=20&m=872670490&s=612x612&w=0&h=2LVHEll6P7oryjVTqcJemHSjuvxersY29Nn5J0BZGik=",
-  "Visión","Nos basamos en la inteligencia artificial y el análisis de sentimiento para determinar si las comunidades de los destinos escogidos por nuestros clientes pueden afectar negativamente a su integridad."),
+  "Visión"," El análisis de sentimiento es una técnica que se apoya en el análisis de textos, la lingüistica y la inteligencia artificial para detectar las intenciones detrás de un mensaje. Nuestro objetivo es basarnos en estas tecnologías para determinar si las comunidades de los destinos escogidos por nuestros clientes pueden afectar negativamente a su integridad."),
   new CardInfoModel("https://images.pexels.com/photos/7479659/pexels-photo-7479659.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  "Valores","Para nosotros siempre serán prioridades la inclusión social, el respeto al individuo, la seguridad y comodidad del cliente, la facilidad de trámites y la accesibilidad de nuestros servicios."),
+  "Valores","Para nosotros siempre serán prioridad las personas. Para ello la inclusión social, el respeto al individuo, seguridad, comodidad, la facilidad de los trámites y la accesibilidad de nuestros servicios serán siempre un hecho para nuestros clientes."),
 ];
 
 //https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_960_720.jpg
