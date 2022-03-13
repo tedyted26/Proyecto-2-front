6e579ef890b804e30315709434e2777aeb6d6dc1
tedyted26 <tedyted26@hotmail.com>
2022-03-13 20:20:58 +0100

    editada una palabra

--- src/app/frame-central/frame-central.component.ts
+++ src/app/frame-central/frame-central.component.ts
@@ -14,7 +14,7 @@ export class FrameCentralComponent implements OnInit {
   new CardInfoModel("https://media.istockphoto.com/photos/concept-picture-id872670490?k=20&m=872670490&s=612x612&w=0&h=2LVHEll6P7oryjVTqcJemHSjuvxersY29Nn5J0BZGik=",
   "Visión"," El análisis de sentimiento es una técnica que se apoya en el análisis de textos, la lingüistica y la inteligencia artificial para detectar las intenciones detrás de un mensaje. Nuestro objetivo es basarnos en estas tecnologías para determinar si las comunidades de los destinos escogidos por nuestros clientes pueden afectar negativamente a su estancia e integridad."),
   new CardInfoModel("https://images.pexels.com/photos/7479659/pexels-photo-7479659.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  "Valores","Para nosotros siempre serán prioridad las personas. Para ello la inclusión social, el respeto al individuo, seguridad, comodidad, la facilidad de los trámites y la accesibilidad de nuestros servicios serán siempre un hecho para nuestros clientes."),
+  "Valores","Para nosotros siempre serán prioridad las personas. Para ello la inclusión social, el respeto al individuo, seguridad, comodidad, la facilidad de los trámites y la accesibilidad de nuestros servicios serán siempre una garantía para nuestros clientes."),
 ];
 
 //https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_960_720.jpg
