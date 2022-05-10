eb5bbb13afe612c6aed1e21ea6645846c0e93d19
Andres Perez <afperez772@gmail.com>
2022-05-11 01:51:49 +0200

    Ultimos Cambios Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FrameCentralComponent } from './frame-central/frame-central.component';
-<<<<<<< HEAD
+
 import { LoginComponent } from './login/login.component';
-=======
+
 import { MenuAdminComponent } from './menu-admin/menu-admin.component';
->>>>>>> b8ad584e8579d944a10ca15d142c4a616253e66b
 import { PaginaError404Component } from './pagina-error404/pagina-error404.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 import { RegistroComponent } from './registro/registro.component';
@@ -17,14 +16,12 @@ const routes: Routes = [
    { path: 'Sentimiento', component: SentimentAnalysisComponent},
    { path: 'QuienesSomos', component: QuienesSomosComponent},
    { path: 'Error', component: PaginaError404Component},
-<<<<<<< HEAD
+
    { path: 'Login', component: LoginComponent},
    { path: 'Registro', component: RegistroComponent},
-=======
-   { path: 'Admin', component: MenuAdminComponent},
->>>>>>> b8ad584e8579d944a10ca15d142c4a616253e66b
-];
 
+   { path: 'Admin', component: MenuAdminComponent},
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
