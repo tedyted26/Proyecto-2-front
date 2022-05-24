8752376e8592a74bbb19b17b91f09d5781767e10
nerfshacopls <dsjgdavid@gmail.com>
2022-05-24 18:44:23 +0200

    Conexion con back laravel

--- src/app/sentiment-title/sentiment-title.component.ts
+++ src/app/sentiment-title/sentiment-title.component.ts
@@ -3,6 +3,9 @@ import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith, debounceTime } from 'rxjs/operators';
 
+import { SentimentAnalysisService } from '../services/sentiment-analysis.service';
+import { Data } from '@angular/router';
+
 @Component({
   selector: 'app-sentiment-title',
   templateUrl: './sentiment-title.component.html',
@@ -11,11 +14,12 @@ import { map, startWith, debounceTime } from 'rxjs/operators';
 export class SentimentTitleComponent implements OnInit {
 
   array_prueba: string[] = ['White', 'Black', 'Green', 'Blue', 'Yellow', 'Red'];
-
+  puebloSeleccionado = "Kimtone"
+  respuesta = ""
   control = new FormControl();
   filArray!: Observable<string[]>;
 
-  constructor() { }
+  constructor(private sentiment: SentimentAnalysisService) {}
 
   ngOnInit(): void {
     //o aqui (api)
@@ -31,5 +35,21 @@ export class SentimentTitleComponent implements OnInit {
 //si hay llamada a api para pillar los autocompletados hay que pillarlos aqui
     return this.array_prueba.filter(palabra => palabra.toLocaleLowerCase().indexOf(formatVal) === 0);
   }
+
+  //Click boton
+
+  analizarPueblo(){
+    console.log("ANALIZANDO")
+    this.sentiment.getDataFromBackend(this.puebloSeleccionado).subscribe(
+      (response:Data) => {
+        this.respuesta = response["type_text"];
+        console.log(response)
+      },
+      (error:any) => console.log(error),
+      () => console.log("Se ha terminado el proceso")
+    );
+  }
+
+  
 
 }
--- src/app/services/sentiment-analysis.service.ts
+++ src/app/services/sentiment-analysis.service.ts
@@ -11,7 +11,7 @@ export class SentimentAnalysisService {
   constructor(private http: HttpClient) {}
 
   getDataFromBackend(text:String): Observable<Data> {
-     return this.http.post<Data>("http://127.0.0.1:5000/app-sentiment-analysis", {texto: text})
+     return this.http.post<Data>("http://127.0.0.1:8000/api/busquedas", {texto: text})
    }
 
 }
