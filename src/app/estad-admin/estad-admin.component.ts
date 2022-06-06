7e72a02bdaa88c6d1a9102c06f93ec907fd4ef19
tedyted26 <tedyted26@hotmail.com>
2022-06-06 23:33:16 +0200

    graficas terminadas, faltan datos de la bbdd

--- src/app/estad-admin/estad-admin.component.ts
+++ src/app/estad-admin/estad-admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Chart, registerables} from 'chart.js';
+import {Chart, registerables, Scale} from 'chart.js';
 
 @Component({
   selector: 'app-estad-admin',
@@ -9,15 +9,37 @@ import {Chart, registerables} from 'chart.js';
 export class EstadAdminComponent implements OnInit {
 
   //DATOS DE LOS CHARTS
-  dataPolaridad = [60, 40];
-  backgroundColorPolaridad = ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.7)'];
-  borderColorPolaridad = ['rgba(255, 99, 132, 0.8)', 'rgba(54, 162, 235)'];
+  dataBusquedas = [60, 40, 60, 40, 60, 40];
+  labelsLocalidad = ["A Coruña","Albacete","Alicante","Almería",
+  "Asturias","Álava","Ávila","Badajoz","Baleares","Barcelona","Burgos","Cantabria",
+  "Castellón","Ceuta","Ciudad Real","Cuenca","Cáceres","Cádiz",
+  "Córdoba","Girona","Granada","Guadalajara","Guipúzcoa","Huelva","Huesca","Jaén",
+  "La Rioja","Las Palmas","León","Lleida","Lugo","Madrid","Melilla","Murcia","Málaga",
+  "Navarra","Ourense","Palencia","Pontevedra","SC. Tenerife","Salamanca","Segovia","Sevilla",
+  "Soria","Tarragona","Teruel","Toledo","Valencia","Valladolid","Vizcaya","Zamora","Zaragoza"];
+  backgroundColorBusquedaLocalidad = ['rgba(255, 99, 132, 0.7)', 
+  'rgba(54, 162, 235, 0.7)', 
+  'rgba(255, 206, 86, 0.7)',];
+  borderColorBusquedaLocalidad = ['rgba(255, 99, 132)', 
+  'rgba(54, 162, 235)',  
+  'rgba(255, 206, 86)',];
+
+  dataNumOdio = [100, 40, 60, 40, 60, 40];
+  backgroundColorOdioLocalidad = [
+  'rgba(75, 192, 192, 0.7)',
+  'rgba(153, 102, 255, 0.7)',
+  'rgba(255, 159, 64, 0.7)'];
+  borderColorOdioLocalidad = [
+  'rgba(75, 192, 192)',
+  'rgba(153, 102, 255)',
+  'rgba(255, 159, 64)'];
 
   constructor() { }
 
   ngOnInit(): void {
     Chart.register(...registerables);
-    this.createChart("busquedas-localidad", this.dataPolaridad, ["Tweets positivos", "Tweets negativos"], this.backgroundColorPolaridad, this.borderColorPolaridad);
+    this.createChart("busquedas-localidad", this.dataBusquedas, this.labelsLocalidad, this.backgroundColorBusquedaLocalidad, this.borderColorBusquedaLocalidad);
+    this.createChart("odio-localidad", this.dataNumOdio, this.labelsLocalidad, this.backgroundColorOdioLocalidad, this.borderColorOdioLocalidad);
   }
 
   createChart(idChart: any, data: Array<Number>, labels:  Array<String>, backgroundColor:  any, borderColor: any){
@@ -30,8 +52,23 @@ export class EstadAdminComponent implements OnInit {
              backgroundColor: backgroundColor,
              borderColor: borderColor,
              borderWidth: 1
-         }]
+         }],
      },
+     options: {
+      indexAxis : 'x',
+      scales : {
+        x: {
+          ticks: {
+            autoSkip : false,
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          display: false,
+        }
+      } 
+     }
  });
  }
 
