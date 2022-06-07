1255e16cb32c58d76bb633c0e3c257e9c764c7a8
tedyted26 <tedyted26@hotmail.com>
2022-06-07 17:04:01 +0200

    diseño login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,6 +15,9 @@ export class LoginComponent implements OnInit {
   email: new FormControl(null, Validators.required),
   password: new FormControl(null, Validators.required),
   });
+
+  textoError="";
+  
   constructor(private authService: AuthServiceService,private tokenStorage: TokenStorageService, private router:Router) { }
   ngOnInit(): void {
     
@@ -24,10 +27,8 @@ export class LoginComponent implements OnInit {
   submitForm(){
     
     if (this.form.invalid){
-      console.log('Form invalido')
-      return;
+      this.textoError="Comprueba los campos.";
     }
-    console.log("Formulario rellenado exitosamente.")
     this.authService
     .login(this.form.get('email')?.value, this.form.get('password')?.value)
     .subscribe((response) => {
